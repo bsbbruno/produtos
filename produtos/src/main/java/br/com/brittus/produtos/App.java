98ba3badc576e67369c824aec11c07253bd2cc16
Bruno Brito <bsb.brunosoares@gmail.com>
2015-01-22 20:51:21 -0200

    Criacao Projeto Produtos

---
+++ produtos/src/main/java/br/com/brittus/produtos/App.java
@@ -0,0 +1,13 @@
+package br.com.brittus.produtos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
